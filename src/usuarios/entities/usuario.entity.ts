7e5af64d96ec85d57647dfc15af8c9ab2a9fd27b
Jessica Rosário <jessica.rosario4@gmail.com>
2025-01-30 14:39:54 -0300

    :ok_hand: style: Alterações de revisão de código

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,38 +1,56 @@
-import { IsEmail, IsNotEmpty, MinLength } from "class-validator"
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({name: "tb_usuarios"})
+@Entity({ name: 'tb_usuarios' })
 export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn() 
-    id: number
+  @Transform(({ value }: TransformFnParams) => {
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+    return '';
+  })
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  nome: string;
 
-    @IsNotEmpty()
-    @Column({length: 255, nullable: false}) 
-    nome: string
+  @IsEmail()
+  @Transform(({ value }: TransformFnParams) => {
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+    return '';
+  })
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  usuario: string;
 
-    @IsEmail()
-    @IsNotEmpty()
-    @Column({ length: 255, nullable: false })
-    usuario: string
+  @MinLength(8)
+  @Transform(({ value }: TransformFnParams) => {
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+    return '';
+  })
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  senha: string;
 
-    @MinLength(8)
-    @IsNotEmpty()
-    @Column({ length: 255, nullable: false }) 
-    senha: string
+  @Column({ length: 5000 })
+  foto: string;
 
-    @Column({ length: 5000 }) 
-    foto: string
+  @Column('decimal', { precision: 10, scale: 2, nullable: false })
+  peso: number;
 
-    @Column('decimal', { precision: 10, scale: 2, nullable: false })
-    peso: number;
+  @Column('decimal', { precision: 10, scale: 2, nullable: false })
+  altura: number;
 
-    @Column('decimal', { precision: 10, scale: 2, nullable: false })
-    altura: number
+  @Column('decimal', { precision: 10, scale: 2, nullable: false })
+  imc: number;
 
-    @Column('decimal', { precision: 10, scale: 2, nullable: false })
-    imc: number
-    
-    @Column({ length: 255 })
-    classificacao: string
+  @Column({ length: 255 })
+  classificacao: string;
 }
--- src/usuarios/services/usuario.service.ts
+++ src/usuarios/services/usuario.service.ts
@@ -1,124 +1,126 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Usuario } from "../entities/usuario.entity";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario } from '../entities/usuario.entity';
 
 @Injectable()
 export class UsuarioService {
-    
-    constructor(
-        @InjectRepository(Usuario)
-        private usuarioRepository: Repository<Usuario>,
-        // private bcrypt: Bcrypt
-    ) { }
+  constructor(
+    @InjectRepository(Usuario)
+    private usuarioRepository: Repository<Usuario>,
+  ) {}
 
-    async findByUsuario(usuario: string): Promise<Usuario | null> {
-        return await this.usuarioRepository.findOne ({
-            where: {
-                usuario: usuario
-            }
-        })
-    }
+  async findByUsuario(usuario: string): Promise<Usuario | null> {
+    return await this.usuarioRepository.findOne({
+      where: {
+        usuario: usuario,
+      },
+    });
+  }
 
-    async findAll(): Promise<Usuario[]> {
-        return await this.usuarioRepository.find();
+  async findAll(): Promise<Usuario[]> {
+    return await this.usuarioRepository.find();
+  }
 
-    }
+  async findById(id: number): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOne({
+      where: {
+        id,
+      },
+    });
 
-    async findById(id: number): Promise<Usuario> {
+    if (!usuario)
+      throw new HttpException(
+        `Usuário com ID ${id} não encontrado`,
+        HttpStatus.NOT_FOUND,
+      );
 
-        const usuario = await this.usuarioRepository.findOne({
-            where: {
-                id
-            }
-        });
+    return usuario;
+  }
 
-        if (!usuario)
-            throw new HttpException(`Usuário com ID ${id} não encontrado`, HttpStatus.NOT_FOUND);
+  async create(usuario: Usuario): Promise<Usuario> {
+    const buscaUsuario = await this.findByUsuario(usuario.usuario);
 
-        return usuario;
+    if (buscaUsuario)
+      throw new HttpException('O Usuario já existe!', HttpStatus.BAD_REQUEST);
 
-    }
+    const calcularIMC = (
+      peso: number,
+      altura: number,
+    ): { imc: number; classificacao: string } => {
+      if (peso <= 0 || altura <= 0) {
+        throw new HttpException(
+          'Peso e altura devem conter valores válidos',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
 
+      const imc = peso / (altura * altura);
+      const imcArredondado = parseFloat(imc.toFixed(2));
 
-    async create(usuario: Usuario): Promise<Usuario> {
-        
-        const buscaUsuario = await this.findByUsuario(usuario.usuario);
+      const classificarIMC = (imc: number): string => {
+        if (imc < 18.5) return 'Abaixo do peso';
+        if (imc < 24.9) return 'Peso normal';
+        if (imc < 29.9) return 'Sobrepeso';
+        if (imc < 34.9) return 'Obesidade Grau I';
+        if (imc < 39.9) return 'Obesidade Grau II';
+        return 'Obesidade Grau III (Mórbida)';
+      };
 
-        if (buscaUsuario)
-            throw new HttpException("O Usuario já existe!", HttpStatus.BAD_REQUEST);
+      const classificacao = classificarIMC(imcArredondado);
 
-        const calcularIMC = (peso: number, altura: number): { imc: number; classificacao: string } => {
-            if (peso <= 0 || altura <= 0) {
-                throw new HttpException('Peso e altura devem conter valores válidos', HttpStatus.BAD_REQUEST);
-            }
-    
-            const imc = peso / (altura * altura);
-            const imcArredondado = parseFloat(imc.toFixed(2));
+      return { imc: imcArredondado, classificacao };
+    };
 
-            const classificarIMC = (imc: number): string => {
-                if (imc < 18.5) return 'Abaixo do peso';
-                if (imc < 24.9) return 'Peso normal';
-                if (imc < 29.9) return 'Sobrepeso';
-                if (imc < 34.9) return 'Obesidade Grau I';
-                if (imc < 39.9) return 'Obesidade Grau II';
-                return 'Obesidade Grau III (Mórbida)';
-            }
-    
-            const classificacao = classificarIMC(imcArredondado);
-    
-            return { imc: imcArredondado, classificacao };
-        };
-    
-        const { imc, classificacao } = calcularIMC(usuario.peso, usuario.altura);
-    
-        usuario.imc = imc;
-        usuario.classificacao = classificacao;
+    const { imc, classificacao } = calcularIMC(usuario.peso, usuario.altura);
 
-        return await this.usuarioRepository.save(usuario);
+    usuario.imc = imc;
+    usuario.classificacao = classificacao;
 
-    }
+    return await this.usuarioRepository.save(usuario);
+  }
 
-    async update(usuario: Usuario): Promise<Usuario> {
+  async update(usuario: Usuario): Promise<Usuario> {
+    await this.findById(usuario.id);
 
-        await this.findById(usuario.id);
+    const buscaUsuario = await this.findByUsuario(usuario.usuario);
 
-        const buscaUsuario = await this.findByUsuario(usuario.usuario);
+    if (buscaUsuario && buscaUsuario.id !== usuario.id)
+      throw new HttpException('O Usuario já existe!', HttpStatus.BAD_REQUEST);
 
-        if (buscaUsuario && buscaUsuario.id !== usuario.id)
-            throw new HttpException("O Usuario já existe!" , HttpStatus.BAD_REQUEST);
+    const calcularIMC = (
+      peso: number,
+      altura: number,
+    ): { imc: number; classificacao: string } => {
+      if (peso <= 0 || altura <= 0) {
+        throw new HttpException(
+          'Peso e altura devem conter valores válidos',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
 
-        const calcularIMC = (peso: number, altura: number): { imc: number; classificacao: string } => {
-            if (peso <= 0 || altura <= 0) {
-                throw new HttpException('Peso e altura devem conter valores válidos', HttpStatus.BAD_REQUEST);
-            }
-    
-            const imc = peso / (altura * altura);
-            const imcArredondado = parseFloat(imc.toFixed(2));
+      const imc = peso / (altura * altura);
+      const imcArredondado = parseFloat(imc.toFixed(2));
 
-            const classificarIMC = (imc: number): string => {
-                if (imc < 18.5) return 'Abaixo do peso';
-                if (imc < 24.9) return 'Peso normal';
-                if (imc < 29.9) return 'Sobrepeso';
-                if (imc < 34.9) return 'Obesidade Grau I';
-                if (imc < 39.9) return 'Obesidade Grau II';
-                return 'Obesidade Grau III (Mórbida)';
-            }
-    
-            const classificacao = classificarIMC(imcArredondado);
-    
-            return { imc: imcArredondado, classificacao };
-        };
-    
-        const { imc, classificacao } = calcularIMC(usuario.peso, usuario.altura);
-    
-        usuario.imc = imc;
-        usuario.classificacao = classificacao;
+      const classificarIMC = (imc: number): string => {
+        if (imc < 18.5) return 'Abaixo do peso';
+        if (imc < 24.9) return 'Peso normal';
+        if (imc < 29.9) return 'Sobrepeso';
+        if (imc < 34.9) return 'Obesidade Grau I';
+        if (imc < 39.9) return 'Obesidade Grau II';
+        return 'Obesidade Grau III (Mórbida)';
+      };
 
-        return await this.usuarioRepository.save(usuario);
+      const classificacao = classificarIMC(imcArredondado);
 
-        // usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha)
-        
-    }
+      return { imc: imcArredondado, classificacao };
+    };
 
+    const { imc, classificacao } = calcularIMC(usuario.peso, usuario.altura);
+
+    usuario.imc = imc;
+    usuario.classificacao = classificacao;
+
+    return await this.usuarioRepository.save(usuario);
+  }
 }
