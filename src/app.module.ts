1bcc6ff508ebdbcfa112d508e7e6a4c7d55fd663
Cauã R. Pereira <7aauac@gmail.com>
2025-01-30 10:32:09 -0300

    :card_file_box: raw: Realizado as configuraçõe inciais

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_NAME || 'fitness',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    process.env.TZ = '-03:00';
+
+    app.useGlobalPipes(new ValidationPipe());
+
+    app.enableCors();
+
+    await app.listen(process.env.PORT ?? 3000);
+
+    console.log(`A aplicação está sendo executada em: ${await app.getUrl()}`);
+  } catch (error) {
+    console.error('Erro ao iniciar a aplicação:', error);
+    process.exit(1);
+  }
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error('Erro deconhecido no bootstrap:', error);
+  process.exit(1);
+});
