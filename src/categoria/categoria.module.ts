efb9db1fa9ba708a3e5d22911d7bb5bb3d7d6cfa
Cauã R. Pereira <7aauac@gmail.com>
2025-03-06 09:31:46 -0300

    :bug: fix: Corrige nome da pasta

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { Categoria } from './entities/categoria.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CategoriaController } from './controller/categoria.controller';
+import { CategoriaController } from './controllers/categoria.controller';
 import { CategoriaService } from './services/categoria.service';
 
 @Module({
